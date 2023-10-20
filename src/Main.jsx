aa48fe3c57c54571c28a1060c5473c326d57a869
Rojan Dhungel <rojandhungel11@gmail.com>
2023-10-20 13:59:41 +0545

    added scroll to top

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import All from "./All";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter } from "react-router-dom";
 
 
@@ -11,6 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
     <BrowserRouter>
+    <ScrollToTop/>
     <All/>
     </BrowserRouter>
 )
